7f56a8eae11444e1da016948232e633df0a1c21d
Sam Manzano <sam.mnzn0@gmail.com>
2025-11-06 15:56:23 +0800

    Added code to answer Cofactor(A) of 2x2 matrix

--- MatrixAdjoint/MatrixAdjoint/matrix2x2.Designer.cs
+++ MatrixAdjoint/MatrixAdjoint/matrix2x2.Designer.cs
@@ -47,6 +47,11 @@
             this.label9 = new System.Windows.Forms.Label();
             this.label10 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
+            this.label11 = new System.Windows.Forms.Label();
+            this.cof_22 = new System.Windows.Forms.TextBox();
+            this.cof_21 = new System.Windows.Forms.TextBox();
+            this.cof_12 = new System.Windows.Forms.TextBox();
+            this.cof_11 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // row1col1
@@ -55,7 +60,7 @@
             this.row1col1.AccessibleDescription = "";
             this.row1col1.Location = new System.Drawing.Point(102, 118);
             this.row1col1.Name = "row1col1";
-            this.row1col1.Size = new System.Drawing.Size(56, 21);
+            this.row1col1.Size = new System.Drawing.Size(56, 26);
             this.row1col1.TabIndex = 0;
             this.row1col1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.row1col1_KeyPress);
             // 
@@ -65,9 +70,9 @@
             this.row1col2.AccessibleDescription = "";
             this.row1col2.Location = new System.Drawing.Point(164, 118);
             this.row1col2.Name = "row1col2";
-            this.row1col2.Size = new System.Drawing.Size(56, 21);
+            this.row1col2.Size = new System.Drawing.Size(56, 26);
             this.row1col2.TabIndex = 1;
-            this.row1col2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.row2col1_KeyPress);
+            this.row1col2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.row1col1_KeyPress);
             // 
             // row2col1
             // 
@@ -75,9 +80,9 @@
             this.row2col1.AccessibleDescription = "";
             this.row2col1.Location = new System.Drawing.Point(102, 146);
             this.row2col1.Name = "row2col1";
-            this.row2col1.Size = new System.Drawing.Size(56, 21);
+            this.row2col1.Size = new System.Drawing.Size(56, 26);
             this.row2col1.TabIndex = 2;
-            this.row2col1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.row2col1_KeyPress);
+            this.row2col1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.row1col1_KeyPress);
             // 
             // row2col2
             // 
@@ -85,9 +90,9 @@
             this.row2col2.AccessibleDescription = "";
             this.row2col2.Location = new System.Drawing.Point(164, 146);
             this.row2col2.Name = "row2col2";
-            this.row2col2.Size = new System.Drawing.Size(56, 21);
+            this.row2col2.Size = new System.Drawing.Size(56, 26);
             this.row2col2.TabIndex = 3;
-            this.row2col2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.row2col1_KeyPress);
+            this.row2col2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.row1col1_KeyPress);
             // 
             // var1
             // 
@@ -97,7 +102,7 @@
             this.var1.Location = new System.Drawing.Point(240, 118);
             this.var1.MaxLength = 1;
             this.var1.Name = "var1";
-            this.var1.Size = new System.Drawing.Size(56, 21);
+            this.var1.Size = new System.Drawing.Size(56, 26);
             this.var1.TabIndex = 4;
             this.var1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.var1_KeyPress);
             // 
@@ -109,7 +114,7 @@
             this.var2.Location = new System.Drawing.Point(240, 146);
             this.var2.MaxLength = 1;
             this.var2.Name = "var2";
-            this.var2.Size = new System.Drawing.Size(56, 21);
+            this.var2.Size = new System.Drawing.Size(56, 26);
             this.var2.TabIndex = 5;
             this.var2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.var1_KeyPress);
             // 
@@ -119,9 +124,9 @@
             this.result1.AccessibleDescription = "";
             this.result1.Location = new System.Drawing.Point(342, 118);
             this.result1.Name = "result1";
-            this.result1.Size = new System.Drawing.Size(56, 21);
+            this.result1.Size = new System.Drawing.Size(56, 26);
             this.result1.TabIndex = 6;
-            this.result1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.row2col1_KeyPress);
+            this.result1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.row1col1_KeyPress);
             // 
             // result2
             // 
@@ -129,27 +134,27 @@
             this.result2.AccessibleDescription = "";
             this.result2.Location = new System.Drawing.Point(342, 146);
             this.result2.Name = "result2";
-            this.result2.Size = new System.Drawing.Size(56, 21);
+            this.result2.Size = new System.Drawing.Size(56, 26);
             this.result2.TabIndex = 7;
-            this.result2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.row2col1_KeyPress);
+            this.result2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.row1col1_KeyPress);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("TT Hoves Pro Trial", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(12, 10);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(164, 30);
+            this.label1.Size = new System.Drawing.Size(234, 40);
             this.label1.TabIndex = 8;
             this.label1.Text = "Enter Matrix:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("TT Hoves Pro Trial", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.Location = new System.Drawing.Point(313, 122);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(13, 14);
+            this.label2.Size = new System.Drawing.Size(19, 20);
             this.label2.TabIndex = 9;
             this.label2.Text = "=";
             this.label2.Click += new System.EventHandler(this.label2_Click);
@@ -157,10 +162,10 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("TT Hoves Pro Trial", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.Location = new System.Drawing.Point(313, 150);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(13, 14);
+            this.label3.Size = new System.Drawing.Size(19, 20);
             this.label3.TabIndex = 10;
             this.label3.Text = "=";
             this.label3.Click += new System.EventHandler(this.label3_Click);
@@ -168,10 +173,10 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("TT Hoves Pro Trial", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.Location = new System.Drawing.Point(244, 89);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(46, 14);
+            this.label4.Size = new System.Drawing.Size(70, 20);
             this.label4.TabIndex = 11;
             this.label4.Text = "Variable";
             this.label4.Click += new System.EventHandler(this.label4_Click);
@@ -179,10 +184,10 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("TT Hoves Pro Trial", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.Location = new System.Drawing.Point(120, 89);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(14, 14);
+            this.label5.Size = new System.Drawing.Size(20, 20);
             this.label5.TabIndex = 12;
             this.label5.Text = "A";
             this.label5.Click += new System.EventHandler(this.label5_Click);
@@ -190,10 +195,10 @@
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Font = new System.Drawing.Font("TT Hoves Pro Trial", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label6.Location = new System.Drawing.Point(182, 89);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(14, 14);
+            this.label6.Size = new System.Drawing.Size(21, 20);
             this.label6.TabIndex = 13;
             this.label6.Text = "B";
             this.label6.Click += new System.EventHandler(this.label6_Click);
@@ -201,20 +206,20 @@
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Font = new System.Drawing.Font("TT Hoves Pro Trial", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label7.Location = new System.Drawing.Point(137, 62);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(44, 14);
+            this.label7.Size = new System.Drawing.Size(66, 20);
             this.label7.TabIndex = 14;
             this.label7.Text = "Column";
             // 
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Font = new System.Drawing.Font("TT Hoves Pro Trial", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label8.Location = new System.Drawing.Point(67, 122);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(11, 14);
+            this.label8.Size = new System.Drawing.Size(18, 20);
             this.label8.TabIndex = 15;
             this.label8.Text = "1";
             this.label8.Click += new System.EventHandler(this.label8_Click);
@@ -222,10 +227,10 @@
             // label9
             // 
             this.label9.AutoSize = true;
-            this.label9.Font = new System.Drawing.Font("TT Hoves Pro Trial", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label9.Location = new System.Drawing.Point(67, 150);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(13, 14);
+            this.label9.Size = new System.Drawing.Size(18, 20);
             this.label9.TabIndex = 16;
             this.label9.Text = "2";
             this.label9.Click += new System.EventHandler(this.label9_Click);
@@ -233,10 +238,10 @@
             // label10
             // 
             this.label10.AutoSize = true;
-            this.label10.Font = new System.Drawing.Font("TT Hoves Pro Trial", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label10.Location = new System.Drawing.Point(23, 136);
             this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(29, 14);
+            this.label10.Size = new System.Drawing.Size(42, 20);
             this.label10.TabIndex = 17;
             this.label10.Text = "Row";
             this.label10.Click += new System.EventHandler(this.label10_Click);
@@ -249,12 +254,60 @@
             this.button1.TabIndex = 18;
             this.button1.Text = "Calculate";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(34, 262);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(146, 29);
+            this.label11.TabIndex = 23;
+            this.label11.Text = "Cofactor (A):";
+            // 
+            // cof_22
+            // 
+            this.cof_22.Location = new System.Drawing.Point(276, 312);
+            this.cof_22.Name = "cof_22";
+            this.cof_22.ReadOnly = true;
+            this.cof_22.Size = new System.Drawing.Size(56, 26);
+            this.cof_22.TabIndex = 22;
+            // 
+            // cof_21
+            // 
+            this.cof_21.Location = new System.Drawing.Point(202, 312);
+            this.cof_21.Name = "cof_21";
+            this.cof_21.ReadOnly = true;
+            this.cof_21.Size = new System.Drawing.Size(56, 26);
+            this.cof_21.TabIndex = 21;
+            // 
+            // cof_12
+            // 
+            this.cof_12.Location = new System.Drawing.Point(276, 266);
+            this.cof_12.Name = "cof_12";
+            this.cof_12.ReadOnly = true;
+            this.cof_12.Size = new System.Drawing.Size(56, 26);
+            this.cof_12.TabIndex = 20;
+            // 
+            // cof_11
+            // 
+            this.cof_11.Location = new System.Drawing.Point(202, 266);
+            this.cof_11.Name = "cof_11";
+            this.cof_11.ReadOnly = true;
+            this.cof_11.Size = new System.Drawing.Size(56, 26);
+            this.cof_11.TabIndex = 19;
             // 
             // matrix2x2
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 14F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(433, 245);
+            this.ClientSize = new System.Drawing.Size(433, 461);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.cof_22);
+            this.Controls.Add(this.cof_21);
+            this.Controls.Add(this.cof_12);
+            this.Controls.Add(this.cof_11);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label10);
             this.Controls.Add(this.label9);
@@ -274,7 +327,7 @@
             this.Controls.Add(this.row2col1);
             this.Controls.Add(this.row1col2);
             this.Controls.Add(this.row1col1);
-            this.Font = new System.Drawing.Font("TT Hoves Pro Trial", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Name = "matrix2x2";
             this.Text = "2 x 2 Matrix Calculator";
             this.Load += new System.EventHandler(this.matrix2x2_Load);
@@ -304,5 +357,10 @@
         private System.Windows.Forms.Label label9;
         private System.Windows.Forms.Label label10;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox cof_22;
+        private System.Windows.Forms.TextBox cof_21;
+        private System.Windows.Forms.TextBox cof_12;
+        private System.Windows.Forms.TextBox cof_11;
     }
 }
--- MatrixAdjoint/MatrixAdjoint/matrix2x2.cs
+++ MatrixAdjoint/MatrixAdjoint/matrix2x2.cs
@@ -62,6 +62,7 @@ namespace MatrixAdjoint
 
         }
 
+        // Function for input validation
         private void row1col1_KeyPress(object sender, KeyPressEventArgs e)
         {
             if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && (e.KeyChar != '.'))
@@ -76,26 +77,29 @@ namespace MatrixAdjoint
             }
         }
 
-        private void row2col1_KeyPress(object sender, KeyPressEventArgs e)
-        {
-            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && (e.KeyChar != '.'))
-            {
-                e.Handled = true;
-            }
-
-            // only allow one decimal point
-            if ((e.KeyChar == '.') && ((sender as TextBox).Text.IndexOf('.') > -1))
-            {
-                e.Handled = true;
-            }
-        }
-
+        // Function for input validation of variable input
         private void var1_KeyPress(object sender, KeyPressEventArgs e)
         {
             if (!Char.IsLetter(e.KeyChar) && !char.IsControl(e.KeyChar))
             {
                 e.Handled = true;
             }
+        }
+
+        // Function to calculate for the solutions
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Get all text values from the matrix and convert to double
+            double r1c1 = Convert.ToDouble(row1col1.Text);
+            double r1c2 = Convert.ToDouble(row1col2.Text);
+            double r2c1 = Convert.ToDouble(row2col1.Text);
+            double r2c2 = Convert.ToDouble(row2col2.Text);
+
+            // Solve for Cofactor [A]
+            cof_11.Text = Convert.ToString(r2c2);
+            cof_12.Text = Convert.ToString(-1 * r2c1); // r2c1
+            cof_21.Text = Convert.ToString(-1 * r1c2); // r1c2
+            cof_22.Text = Convert.ToString(r1c1);
         }
     }
 }
